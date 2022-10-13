22c949cc3a6df5f3f409b0712aff822e4e49f5c2
Cyril Plisko <cyril.plisko@mountall.com>
2022-10-13 13:53:49 +0300

    Add apiregistrationv1

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub use openapi::api::storage::v1 as storagev1;
 pub use openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1 as apiextensionsv1;
 pub use openapi::apimachinery::pkg::apis::meta::v1 as metav1;
 pub use openapi::apimachinery::pkg::util::intstr;
+pub use openapi::kube_aggregator::pkg::apis::apiregistration::v1 as apiregistrationv1;
 pub use openapi::ByteString;
 pub use openapi::Resource;
 
