40462f4cbc2e2784c5c7723e04a171543d9f247d
Cyril Plisko <cyril.plisko@mountall.com>
2024-10-07 10:01:09 +0300

    More NodeExt methods

--- src/ext/node.rs
+++ src/ext/node.rs
@@ -7,6 +7,14 @@ pub trait NodeExt: ResourceBuilder {
     /// Creates new `corev1::Node object with given `name`
     ///
     fn new(name: impl ToString) -> Self;
+
+    /// Sets node taints
+    ///
+    fn taints(self, taints: impl IntoIterator<Item = corev1::Taint>) -> Self;
+
+    /// Marks node as unschedulable
+    ///
+    fn unschedulable(self) -> Self;
 }
 
 impl NodeExt for corev1::Node {
@@ -18,5 +26,24 @@ impl NodeExt for corev1::Node {
             // status: todo!(),
             ..default()
         }
+    }
+
+    fn taints(mut self, taints: impl IntoIterator<Item = corev1::Taint>) -> Self {
+        let taints = taints.into_iter().collect();
+        self.spec_mut().taints = Some(taints);
+        self
+    }
+
+    fn unschedulable(mut self) -> Self {
+        self.spec_mut().unschedulable = Some(true);
+        self
+    }
+}
+
+impl HasSpec for corev1::Node {
+    type Spec = corev1::NodeSpec;
+
+    fn spec_mut(&mut self) -> &mut Self::Spec {
+        self.spec.get_or_insert_with(default)
     }
 }
