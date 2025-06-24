fdddae02552feeafb7c2732fcd84cb39ce3f5f18
Cyril Plisko <cyril.plisko@mountall.com>
2025-06-24 12:55:27 +0300

    Update PodSpecExt

--- src/ext/pod_spec.rs
+++ src/ext/pod_spec.rs
@@ -1,20 +1,40 @@
 use super::*;
 
 pub trait PodSpecExt {
+    /// Build `corev1::PodSpec` with this container
+    ///
     fn container(container: corev1::Container) -> Self;
 
+    /// Build `corev1::PodSpec` with these containers
+    ///
     fn containers(containers: impl IntoIterator<Item = corev1::Container>) -> Self;
 
+    /// Set service account name
+    ///
     fn service_account_name(self, name: impl ToString) -> Self;
 
+    /// Set image pull secret
+    ///
     fn image_pull_secret(self, name: impl ToString) -> Self;
 
+    /// Add `volumes`
+    ///
     fn volumes(self, volumes: impl IntoIterator<Item = corev1::Volume>) -> Self;
 
+    /// Add node selector
+    ///
     fn node_selector(
         self,
         node_selector: impl IntoIterator<Item = (impl ToString, impl ToString)>,
     ) -> Self;
+
+    /// Set affinity
+    ///
+    fn affinity(self, affinity: impl Into<Option<corev1::Affinity>>) -> Self;
+
+    /// Add tolerations
+    ///
+    fn tolerations(self, tolerations: impl IntoIterator<Item = corev1::Toleration>) -> Self;
 }
 
 impl PodSpecExt for corev1::PodSpec {
@@ -76,33 +96,37 @@ impl PodSpecExt for corev1::PodSpec {
         }
     }
 
-    fn image_pull_secret(self, name: impl ToString) -> Self {
-        let image_pull_secret_name = corev1::LocalObjectReference::new(name);
-        let image_pull_secrets = Some(vec![image_pull_secret_name]);
-        Self {
-            image_pull_secrets,
-            ..self
-        }
+    fn image_pull_secret(mut self, name: impl ToString) -> Self {
+        let secret = corev1::LocalObjectReference::new(name);
+        self.image_pull_secrets.get_or_insert_default().push(secret);
+        self
     }
 
-    fn volumes(self, volumes: impl IntoIterator<Item = corev1::Volume>) -> Self {
-        let volumes = Some(volumes.into_iter().collect());
-        Self { volumes, ..self }
+    fn volumes(mut self, volumes: impl IntoIterator<Item = corev1::Volume>) -> Self {
+        self.volumes.get_or_insert_default().extend(volumes);
+        self
     }
 
     fn node_selector(
-        self,
+        mut self,
         node_selector: impl IntoIterator<Item = (impl ToString, impl ToString)>,
     ) -> Self {
-        let node_selector = Some(
-            node_selector
-                .into_iter()
-                .map(|(key, value)| (key.to_string(), value.to_string()))
-                .collect(),
-        );
-        Self {
-            node_selector,
-            ..self
-        }
+        let node_selector = node_selector
+            .into_iter()
+            .map(|(key, value)| (key.to_string(), value.to_string()));
+        self.node_selector
+            .get_or_insert_default()
+            .extend(node_selector);
+        self
+    }
+
+    fn affinity(self, affinity: impl Into<Option<corev1::Affinity>>) -> Self {
+        let affinity = affinity.into();
+        Self { affinity, ..self }
+    }
+
+    fn tolerations(mut self, tolerations: impl IntoIterator<Item = corev1::Toleration>) -> Self {
+        self.tolerations.get_or_insert_default().extend(tolerations);
+        self
     }
 }
